a02569a8c37fd6c4379a5177595ac9aa3bca2d3a
netotz <neto.otz@hotmail.com>
2026-08-20 20:58:01 -0700

    add unit test

--- chapter2/topic14/exercise4/tests/parser_test.py
+++ chapter2/topic14/exercise4/tests/parser_test.py
@@ -1,8 +1,39 @@
-from ..instruction import Instruction
+from pathlib import Path
 
 import pytest
 
+from exercise4.parser import parse_file, COMMANDS
+from exercise4.instruction import Instruction
 
-@pytest.mark.parametrize("filepath, expected_instructions", [])
+DATA_DIR = Path(__file__).parent / "data"
+
+
+@pytest.mark.parametrize(
+    ("filepath", "expected_instructions"),
+    [
+        (
+            DATA_DIR / "test1.txt",
+            [
+                Instruction(
+                    COMMANDS["D"],
+                ),
+                Instruction(
+                    COMMANDS["U"],
+                ),
+            ],
+        ),
+    ],
+)
 def test_parser(filepath: str, expected_instructions: list[Instruction]):
-    pass
+    actual = parse_file(filepath)
+
+    # requires overriding comparing method
+    # assert actual == expected_instructions
+
+    assert len(actual) == len(expected_instructions)
+
+    for actual_inst, expected_inst in zip(actual, expected_instructions):
+        assert (
+            actual_inst.command.key == expected_inst.command.key
+            and actual_inst.argument == expected_inst.argument
+        )
